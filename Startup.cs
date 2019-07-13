da4ace25a5bf4eb36a92d8ec051a690c063f4046
alexis-acosta <44756330+alexis-acosta@users.noreply.github.com>
2019-07-13 13:40:43 -0700

    angular integration
    the back end still functions the same, but I am trying to add angular integration to it. At the moment, the angular client does not show up on the web page and I am not sure why.

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using ValkyrieIMS.Models;
 
 namespace ValkyrieIMS
@@ -37,7 +38,9 @@ namespace ValkyrieIMS
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
